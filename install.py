f2ce448d8273e07721fc03ce28e2c2f2a32b6643
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2021-11-01 18:33:42 +0200

    Update install.py

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import sqlite3
 import subprocess
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
-#bashirlink = input("Please Enter Raw data link")
+bashirlink = 'https://raw.githubusercontent.com/nocturnalarchives/BlockLists/master/regex-blacklist-special-rules.txt'
 
 def fetch_url(url):
 
@@ -36,7 +36,7 @@ def fetch_url(url):
     return response
 
 
-url_regexps_remote = 'https://raw.githubusercontent.com/BashirAljounaidy/pihole-regex/main/regex.list'
+url_regexps_remote = bashirlink
 install_comment = 'github.com/BashirAljounaidy/pihole-regex'
 
 cmd_restart = ['pihole', 'restartdns', 'reload']
